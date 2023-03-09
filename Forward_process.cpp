1f87f6196e529ff058ef9b21667d42ae4053ceee
zhouhao <593109442@qq.com>
2023-03-09 20:02:39 +0800

    2

--- Forward_process.cpp
+++ Forward_process.cpp
@@ -1,38 +1,6 @@
 #include "Refractive.h"
-#include <iostream>
-#include <math.h>
-#include <fstream>
-#include <sstream>
-#include <string.h>
-//#include <mpi.h>
 
-using namespace std;
-
-extern string Element;
-
-string inttoStr(int s)
-{
-	string c = "";
-
-	int m = s;
-	int n=0;
-
-	while(m>0)
-	{
-		n++;
-		m/=10;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-			c = (char)(s % 10 + 48) + c;
-			s /= 10;
-	}
-	return c;
-
-}
-
-double lifetime(string Element, double rho)
+double Refractive::lifetime(string Element, double rho)
 {
 	double t = 0;
 	double r, p1, p2, p3, p4, p5, p6, p7;
@@ -94,82 +62,6 @@ double lifetime(string Element, double rho)
 	return t;
 }
 
-Refractive::Refractive(const char* EXP_file, ExpType file_type)
-:fileType(file_type)
-{
-	std::ifstream input(EXP_file);
-	std::string line;
-	std::vector < std::vector<double>> Data;
-	double code;
-	
-	if (input)//如果有文件
-	{
-		
-		while (std::getline(input, line))//把文件里的内容复制到Data暂时储存
-		{
-			std::istringstream sstream(line);
-			std::vector <double> row;
-			while (sstream >> code)
-				row.push_back(code);
-			Data.push_back(row);
-		}
-
-		//在这考虑的是：因为文件里的时间间隔是相等的，所以用文件中的平均时间间隔作为我们的时间间隔；
-		this->dt = (Data[Data.size()-1][0] - Data[0][0]) / (Data.size()-1);
-
-		//那么我们就可以将数据按顺序填入
-		switch (this->fileType)
-		{
-			case REFRACTIVE_DATA: //如果EXP_file文件里的第二列已经是折射率数据的话
-				for (unsigned int i = 0; i < Data.size(); ++i)
-				{
-					this->Flux_Data.push_back(0);
-					this->EXP_Data.push_back(Data[i][1]);
-				}
-				std::cout << "折射率数据加载完毕！" << std::endl;
-				break;
-			case REFLECTIVE_DATA://如果EXP_file文件里的第二列已经是反射率数据的话
-				for (unsigned int i = 0; i < Data.size(); ++i)
-				{
-					this->Flux_Data.push_back(0);
-					this->EXP_Data.push_back(Data[i][1]);
-				}
-				std::cout << "反射率数据加载完毕！" << std::endl;
-				break;
-			case NORMALIZED_DATA://如果EXP_file文件里第二列已经是相对光强数据的话
-				for (unsigned int i = 0; i < Data.size(); ++i)
-				{
-					this->Flux_Data.push_back(0);
-					this->EXP_Data.push_back(Data[i][1]);
-				}
-				std::cout << "相对光强数据加载完毕！" << std::endl;
-				break;
-			case PHASESHIFT_DATA://如果EXP_file文件里第二列已经是相位变化数据的话
-				for (unsigned int i = 0; i < Data.size(); ++i)
-				{
-					this->Flux_Data.push_back(0);
-					this->EXP_Data.push_back(Data[i][1]);
-				}
-				std::cout << "相位变化数据加载完毕！" << std::endl;
-				break;
-			default:
-				break;
-		}
-
-		//初始化基本参数
-		//初始X射线通量J/m^2
-	}
-	else
-	{
-		std::cout << "Data文件没找到或没数据！请再查看你的文件名！" << std::endl;
-	}
-}
-
-void Refractive::measure(const char* EXP_file, ExpType EXP_type, const char* Flux_file)
-{
-	std::cout << "抱歉，还没有完成这方面的算法呢" << std::endl;
-}
-
 void Refractive::Forward_process(const char* Output_file,int jishu, double Energy,double Sample_thickness, double dx)
 {
 	ifstream filein;
@@ -221,10 +113,6 @@ void Refractive::Forward_process(const char* Output_file,int jishu, double Energ
     filein >> p;
 
     cout<<  a[1]<<endl;
-
-
-
-
 
     filein.close();
 
